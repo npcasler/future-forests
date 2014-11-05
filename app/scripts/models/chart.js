610261fdde6e91e45ba8bd2bfd8e74670788d476
dguaderrama <dguaderrama@email.arizona.edu>
2014-11-04 18:53:06 -0700

    Updated charts.hbs and chart.js

--- app/scripts/models/chart.js
+++ app/scripts/models/chart.js
@@ -9,14 +9,91 @@ Forests.Chart.FIXTURES = [{
 
   id: 1,
   name: 'Chart1',
-  pictureUrl: '../images/aspen-current.jpg',
-  figureUrl: '../images/Juniperus_osteosperma.jpg',
+  pictureUrl: '../images/chart_abies_lasiocarpa.jpg',
+  figureUrl: '../images/Abies_lasiocarpa.jpg',
 
 }, {
 
   id: 2,
   name: 'Chart2',
-  pictureUrl: '../images/aspen26.jpg',
-  figureUrl: '../images/Abies_lasiocarpa.jpg'
+  pictureUrl: '../images/chart_picea_engelmannii.jpg',
+  figureUrl: '../images/Picea_engelmannii.jpg'
+
+}, {
+
+  id: 3,
+  name: 'Chart3',
+  pictureUrl: '../images/chart_pinus_contorta.jpg',
+  figureUrl: '../images/Pinus_contorta.jpg'
+
+}, {
+
+  id: 4,
+  name: 'Chart4',
+  pictureUrl: '../images/chart_acer_glabrum.jpg',
+  figureUrl: '../images/Acer_glabrum.jpg'
+
+}, {
+
+  id: 5,
+  name: 'Chart5',
+  pictureUrl: '../images/chart_pinus_ponderosa.jpg',
+  figureUrl: '../images/Pinus_ponderosa.jpg'
+
+}, {
+
+  id: 6,
+  name: 'Chart6',
+  pictureUrl: '../images/chart_pinus_jeffreyi.jpg',
+  figureUrl: '../images/Pinus_jeffreyi.jpg'
+
+}, {
+
+  id: 7,
+  name: 'Chart7',
+  pictureUrl: '../images/chart_pinus_edulis.jpg',
+  figureUrl: '../images/Pinus_edulis.jpg'
+
+}, {
+
+  id: 8,
+  name: 'Chart8',
+  pictureUrl: '../images/chart_pseudotsuga_menziesii.jpg',
+  figureUrl: '../images/Pseudotsuga_menziesii.jpg'
+
+}, {
+
+  id: 9,
+  name: 'Chart9',
+  pictureUrl: '../images/chart_tsuga_heterophylla.jpg',
+  figureUrl: '../images/Tsuga_heterohpylla.jpg'
+
+}, {
+
+  id: 10,
+  name: 'Chart10',
+  pictureUrl: '../images/chart_acacia_greggii.jpg',
+  figureUrl: '../images/Acacia_greggii.jpg'
+
+}, {
+
+  id: 11,
+  name: 'Chart11',
+  pictureUrl: '../images/chart_olneya_tesota.jpg',
+  figureUrl: '../images/Olneya_tesota.jpg'
+
+}, {
+
+  id: 12,
+  name: 'Chart12',
+  pictureUrl: '../images/chart_quercus_gambelii.jpg',
+  figureUrl: '../images/Quercus_gambelii.jpg'
+
+}, {
+
+  id: 13,
+  name: 'Chart13',
+  pictureUrl: '../images/chart_juniperus_osteosperma.jpg',
+  figureUrl: '../images/Juniperus_osteosperma.jpg'
 
 }];
